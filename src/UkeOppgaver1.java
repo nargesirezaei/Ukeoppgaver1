79bf319442cd160cb2121293458ac35cdad804b7
nargesirzi <71456874+nargesirezaei@users.noreply.github.com>
2021-09-03 11:54:16 +0200

    Initial commit

---
+++ src/UkeOppgaver1.java
@@ -0,0 +1,13 @@
+public class UkeOppgaver1 {
+    public static void main (String[]args){
+        System.out.println("hello");
+        int [] a= {8,4,17,10,6,20,1,11,15,3,18,9,2,7,19};
+        int min_index= min(a);
+        System.out.println(min_index);
+    }
+         static int min (int [] a){
+            int in_value = a[0];
+            int min_index = 0;
+            return min_index;
+    }
+}
